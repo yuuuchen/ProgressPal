783b1289bd7db96b05e3619d195a7e6ea5a26a23
yuuuchen <s112029023@smail.nchu.edu.tw>
2025-10-20 21:38:29 +0800

    update learnig/models.py and learning/admin.py

--- progresspal/learning/admin.py
+++ progresspal/learning/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Chapter, Unit
 
 # Register your models here.
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('chapter_number', 'title')
+
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('chapter','unit_number', 'title' )
+
+admin.site.register(Chapter,ChapterAdmin)
+admin.site.register(Unit,UnitAdmin)
--- progresspal/learning/models.py
+++ progresspal/learning/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Chapter(models.Model):
+    chapter_number = models.PositiveIntegerField(unique=True, verbose_name="章節數字")
+    title = models.CharField(max_length=50, verbose_name="章節標題")
+
+    class Meta:
+        ordering = ['chapter_number'] #依照章節數字排序
+
+    def __str__(self):
+        return f"{self.chapter_number}. {self.title}"
+    
+class Unit(models.Model):
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name='units', verbose_name="所屬章節")
+    unit_number = models.CharField(max_length=10, verbose_name="單元編號") 
+    title = models.CharField(max_length=50, verbose_name="單元標題")
+
+    class Meta:
+        unique_together = ('chapter', 'unit_number') #確保在同一個章節內的單元編號是唯一的
+        ordering = ['unit_number'] #依照單元數字排序
+
+    def __str__(self):
+        return f"{self.unit_number} {self.title}"
