1133249610f555821d0bdcef0ee65ec0d5ea86e3
ruthtsai <94ruthtsai@gmail.com>
2025-11-18 01:30:58 +0800

    調整admin顯示

--- progresspal/learning/admin.py
+++ progresspal/learning/admin.py
@@ -7,6 +7,9 @@ class ChapterAdmin(admin.ModelAdmin):
 
 class UnitAdmin(admin.ModelAdmin):
     list_display = ('chapter','unit_number', 'title' )
+    ordering = ('chapter__chapter_number', 'unit_number')
+    list_filter = ('chapter',)
+
 
 admin.site.register(Chapter,ChapterAdmin)
 admin.site.register(Unit,UnitAdmin)
